ee5942994bdde998fa8d1f4a33be6b9fa7b7ff12
Marwa <marwa.ahmad8@gmail.com>
2021-01-03 22:50:50 +0200

    Added OrderService

---
+++ src/Insurance.Service/Interfaces/IOrderInsuranceService.cs
@@ -0,0 +1,11 @@
+﻿
+using Insurance.Domain;
+using System.Threading.Tasks;
+
+namespace Insurance.Service
+{
+    public interface IOrderInsuranceService
+    {
+        Task<OrderInsuranceResponseDto> GetOrderInsuranceAsync(OrderInsuranceRequestDto request);
+    }
+}
---
+++ src/Insurance.Service/Interfaces/IProductInsuranceService.cs
@@ -0,0 +1,11 @@
+﻿using Insurance.Domain;
+using System.Threading.Tasks;
+
+namespace Insurance.Service
+{
+    public interface IProductInsuranceService
+    {
+        Task<InsuranceResponseDto> GetProductInsuranceAsync(int productId);
+        Task<ProductInsurance> GetProductInsuranceDetailsAsync(int productId);
+    }
+}
