2845995e6f71d1d9492a6e01c19b0b138d9669cc
Marwa <marwa.ahmad8@gmail.com>
2021-01-03 19:48:55 +0200

    Added order insurance dtos and validation

---
+++ src/Insurance.Domain/Dtos/OrderInsuranceRequestDto.cs
@@ -0,0 +1,14 @@
+﻿
+using Insurance.Domain.ValidationExtension;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Insurance.Domain
+{
+    public class OrderInsuranceRequestDto
+    {
+        [JsonPropertyName("productsIds")]
+        [CannotBeEmtyList]
+        public IList<int> ProductsIds { get; set; }
+    }
+}
---
+++ src/Insurance.Domain/ValidationExtension/CannotBeEmtyListAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel.DataAnnotations;
+
+namespace Insurance.Domain.ValidationExtension
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class CannotBeEmtyListAttribute : RequiredAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            var enumerable = value as IEnumerable;
+            return enumerable != null && enumerable.GetEnumerator().MoveNext();
+        }
+    }
+}
