863cdf7d81d7e555dcef67818caa931dfb81d9ba
Marwa <marwa.ahmad8@gmail.com>
2021-01-05 01:26:25 +0200

    Added unit tests for product type surcharge rate.

---
+++ tests/Insurance.Tests/Controllers/SurchargeRateControllerUnitTest.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Xunit;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Insurance.Tests.Controllers
+{
+    /// <summary>
+    /// Two approaches to assign surcharge rate to a product type:
+    /// 1. simple choice in three stpes:(preferred; because of the separation of concern, high maintainability, decoupled solution)
+    /// 1.a. create endpoint for creating surcharge rates; Name, Value --> returns the Id of the surcharge rate created or the whole object created
+    /// 1.b. create endpoint to list all surcharge rates; Id, Name, Value
+    /// 1.c. create endpoint to assign product type to its surcharge rates; productTypeId, surchargeRatesIds
+    /// 
+    /// 2. one single step
+    /// 2.a CreateSurchareRateProductType; send productId with list of floats which are surchargeRates
+    /// Cons of 2nd choice is: 
+    ///     could add add multiple surcharge rates with the same value
+    ///     not extendable functionality; not easy to maintain
+    /// 
+    /// </summary>
+    public class SurchargeRateControllerUnitTest : IClassFixture<SetupTestFixture>
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private SurchargeRateController _surchargeRateController;
+
+        public SurchargeRateControllerUnitTest(SetupTestFixture setuptestFixture)
+        {
+            _serviceProvider = setuptestFixture.ServiceProvider;
+
+            _surchargeRateController = _serviceProvider.GetService<SurchargeRateController>();
+        }
+
+        //[Theory]
+        //[MemberData(nameof(SurchargeRateTestData.CreateDistinctSurchargeRates), MemberType = typeof(SurchargeRateTestData))]
+        //public async Task CreateSurchargeRates_ExpectedCreated(SurchargeRateCreateRequestDto surchargeRateCreateRequestDto, List<string> expectedSurchargeNames)
+        //{
+        //    //we need async data structure for storing the product type and its surcharge rate
+        //    var result = await _surchargeRateController.CreateSurchargeRates(surchargeRateCreateRequestDto);
+        //    Assert.IsType<CreatedResult>(result);
+
+        //    Assert.Equal(
+        //        expected: expectedSurchargeNames,
+        //        actual: ((SurchargeRateCreateResponseDto)((OkObjectResult)result).Value).SurchargeRates.Select(r => r.Name).ToList()
+        //    );
+        //}
+
+        //[Theory]
+        //[MemberData(nameof(SurchargeRateTestData.CreateDistinctSurchargeRates), MemberType = typeof(SurchargeRateTestData))]
+        //public async Task GetSurchargeRates_ExpectedList(SurchargeRateCreateRequestDto surchargeRateCreateRequestDto, List<string> expectedSurchargeNames)
+        //{
+        //    var result = await _surchargeRateController.CreateSurchargeRates(surchargeRateCreateRequestDto);
+        //    Assert.IsType<CreatedResult>(result);
+
+        //    Assert.Equal(
+        //        expected: expectedSurchargeNames,
+        //        actual: ((SurchargeRateCreateResponseDto)((OkObjectResult)result).Value).SurchargeRates.Select(r => r.Name).ToList()
+        //    );
+
+        //    var result = await _surchargeRateController.GetSurchargeRates();
+        //    Assert.IsType<CreatedResult>(result);
+
+        //    Assert.Equal(
+        //        expected: expectedSurchargeNames,
+        //        actual: ((SurchargeRatesGetResponseDto)((OkObjectResult)result).Value).SurchargeRates.Select(r => r.Name).ToList()
+        //    );
+        //}
+
+        [Theory]
+        [MemberData(nameof(SurchargeRateTestData.ValidSurchargeRates), MemberType = typeof(SurchargeRateTestData))]
+        public async Task AddProductTypeSurchargeRates_ExpectedCreated(ProductTypeSurchargeRateRequestDto productTypeSurchargeRateRequestDto)
+        {
+            //we need async data structure for storing the product type and its surcharge rate
+            var result = await _surchargeRateController.CreateProductTypeSurchareRate(productTypeSurchargeRateRequestDto);
+            Assert.IsType<CreatedResult>(result);
+        }
+
+        //[Theory]
+        //[MemberData(nameof(SurchargeRateTestData.TwoProductTypesWithThreeSurchargeRates), MemberType = typeof(SurchargeRateTestData))]
+        //public async Task AddProductTypeSurchargeRatesSingleStep_ExpectedCreated(ProductTypeSurchargeRateRequestDto productTypeSurchargeRateRequestDto)
+        //{
+        //    //we need async data structure for storing the product type and its surcharge rate
+        //    var result = await _surchargeRateController.AddProductTypeSurchargeRates(productTypeSurchargeRateRequestDto);
+        //    Assert.IsType<CreatedResult>(result);
+        //}
+    }
+}
---
+++ tests/Insurance.Tests/TestData/SurchargeRateTestData.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Insurance.Tests
+{
+    public class SurchargeRateTestData
+    {
+        public static IEnumerable<object[]> ValidSurchargeRates = new List<object[]>
+        {
+            new object[]
+            {
+                new ProductTypeSurchargeRateRequestDto()
+                {
+                    ProductTypeId = 33,
+                    SurchareRates = new List<float>(){ 20, 10, 10}
+                }
+            }
+        };
+
+        //public static IEnumerable<object[]> CreateDistinctSurchargeRates = new List<object[]>
+        //{
+        //    new object[]
+        //    {
+        //        new SurchargeRateCreateRequestDto()
+        //        {
+        //            ChargeRates = new List<ChargeRates>()
+        //            {
+        //                new ChargeRates()
+        //                {
+        //                    new SurchargeRate(){Name = "Taxs", Percentage = 12.5f},
+        //                    new SurchargeRate(){Name = "Shipping", Percentage = 3.0f},
+        //                }
+        //            }
+        //        },
+        //        new List<string>(){ "Taxs", "Shipping"}
+        //    }
+        //};
+
+        //public static IEnumerable<object[]> TwoProductTypesWithSurchargeRates_SingleStep = new List<object[]>
+        //{
+        //    new object[]
+        //    {
+        //        new ProductTypeSurchargeRateRequestSingleDto()
+        //        {
+        //            ProductTypesChargeRates = new List<ProductTypeChargeRatesSingle>()
+        //            {
+        //                new ProductTypeChargeRatesSingle()
+        //                {
+        //                    ProductTypeId = 33,
+        //                    SurchareRates = new List<float>(){ 20, 10, 5}
+        //                },
+        //                new ProductTypeChargeRates()
+        //                {
+        //                    ProductTypeId = 21,
+        //                    SurchareRates = new List<float>(){15}
+        //                }
+        //            }
+        //        }
+        //    }
+        //};
+    }
+}
